99c332193c49ed20b27f2826c937d266c366e596
cl-ire <s85609@htw-dresden.de>
2023-11-28 11:49:36 +0100

    fix xml file

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'camera_subscriber_node = camera_package.camera_subscriber:main',
+            'camera_s = camera_package.camera_subscriber_node:main',
         ],
     },
 )
