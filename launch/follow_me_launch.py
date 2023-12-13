0fd64d6e52a3e79887a2f070e48f037b12faef2c
cl-ire <s85609@htw-dresden.de>
2023-12-13 21:15:25 +0100

    fix

--- launch/follow_me_launch.py
+++ launch/follow_me_launch.py
@@ -40,7 +40,7 @@ def generate_launch_description():
             name='v4l2_camera_node',
             output='screen',
             parameters=[
-                {'image_size': [1280,960]},                
+                {'image_size': [640,480]},         
             ],
         ),
 
