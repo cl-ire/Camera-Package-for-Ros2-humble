bbb38fb7d582ccf50b273f85d3fe206077319c03
cl-ire <s85609@htw-dresden.de>
2024-04-15 14:23:23 +0200

    fix

--- camera_package/movement_control_node.py
+++ camera_package/movement_control_node.py
@@ -55,25 +55,26 @@ class MovementControl(Node):
         self.time1        = Position.data[7]
         self.time2        = Position.data[8]
         
-        use_data, old_time = movement_control_util.compare_times(Position, self.old_time)
+        if self.enable_movement:
+            use_data, old_time = movement_control_util.compare_times(Position, self.old_time)
 
-        if use_data:
-            drive_time = 1
-            self.old_time = movement_control_util.add_seconds_to_time(old_time, drive_time)
+            if use_data:
+                drive_time = 1
+                self.old_time = movement_control_util.add_seconds_to_time(old_time, drive_time)
 
-            # Winkelberechnung
-            self.winkel_x = int((self.coordinate_x/self.max_x)*self.max_winkel_x)
-            self.winkel_y = int((self.coordinate_y/self.max_y)*self.max_winkel_y)
-            self.get_logger().info("Angle: [{}, {}]".format(self.winkel_x, self.winkel_y))
+                # Winkelberechnung
+                self.winkel_x = int((self.coordinate_x/self.max_x)*self.max_winkel_x)
+                self.winkel_y = int((self.coordinate_y/self.max_y)*self.max_winkel_y)
+                self.get_logger().info("Angle: [{}, {}]".format(self.winkel_x, self.winkel_y))
 
-            self.servo_msg_hold[0] = self.servo_msg_hold[0] + self.winkel_x
-            self.servo_msg_hold[1] = self.servo_msg_hold[1] + self.winkel_y
+                self.servo_msg_hold[0] = self.servo_msg_hold[0] + self.winkel_x
+                self.servo_msg_hold[1] = self.servo_msg_hold[1] + self.winkel_y
 
-            self.get_logger().info("Data sent to Servo: {}".format(self.servo_msg_hold))
+                self.get_logger().info("Data sent to Servo: {}".format(self.servo_msg_hold))
 
-            servo_msg_sent = Int32MultiArray()
-            servo_msg_sent.data = self.servo_msg_hold
-            self.servo_pub.publish(servo_msg_sent)        
+                servo_msg_sent = Int32MultiArray()
+                servo_msg_sent.data = self.servo_msg_hold
+                self.servo_pub.publish(servo_msg_sent)        
 
 
         try:
