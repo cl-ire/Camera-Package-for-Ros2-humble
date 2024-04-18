00bdb537a75736f154a9a159582f0d8767d7b37d
cl-ire <s85609@htw-dresden.de>
2024-04-18 11:22:43 +0200

    fix

--- camera_package/camera_opencv_loop_node.py
+++ camera_package/camera_opencv_loop_node.py
@@ -13,7 +13,8 @@ from opencv import human_detector
 class CameraOpencv(Node):
     def __init__(self):
         super().__init__('camera_subscriber')
-        self.publisher_ = self.create_publisher(Int32MultiArray, '/position_data', 1)
+        self.publisher_ = self.create_publisher(
+            Int32MultiArray, '/position_data', 1)
         self.image_publisher = self.create_publisher(Image, '/opencv_image', 1)
         self.bridge = CvBridge()
 
@@ -21,7 +22,6 @@ class CameraOpencv(Node):
 
         self.time1
         self.loop()
-
 
     def loop(self):
         while True:
@@ -34,29 +34,30 @@ class CameraOpencv(Node):
                 self.get_logger().info('erro')
 
     def publish_data(self, data):
-                
-        try:            
+
+        try:
             Position, return_image = data
 
             return_image_msg = self.bridge.cv2_to_imgmsg(return_image, "bgr8")    # Image Streamer
             self.image_publisher.publish(return_image_msg)
+
+            if Position != []:
+                Position[7] = self.time1
+                Position[8] = movement_control_util.get_current_time()
+                self.get_logger().info('Position data recived {}'.format(Position))
+                position_data = Int32MultiArray()
+                position_data.data = Position
+                self.publisher_.publish(position_data)
+
         except:
             self.get_logger().info('no Position data recived')
 
-        if Position != []:
-            Position[7] = self.time1
-            Position[8] = movement_control_util.get_current_time()
-            self.get_logger().info('Position data recived {}'.format(Position))
-            position_data = Int32MultiArray()
-            position_data.data = Position
-            self.publisher_.publish(position_data)
-
 
 def main(args=None):
-    
+
     rclpy.init(args=args)
     camera_subscriber = CameraOpencv()  # create node object
-    rclpy.spin(camera_subscriber)       # start node 
+    rclpy.spin(camera_subscriber)       # start node
 
     camera_subscriber.destroy_node()    # destroy node
     rclpy.shutdown()
